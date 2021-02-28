3488e9e0b954b705afae31c992275e8b8799e025
Oskar Person <oskar.person@hotmail.com>
2021-02-28 20:44:55 +0100

    Adding a QuickPolymatrixGame class to serve the callables

--- corpgame.py
+++ corpgame.py
@@ -286,3 +286,29 @@ class GameManager:
         n = self.number_of_players
         strategy_profile = [random.randint(0, 1) for i in range(n)]
         return strategy_profile
+
+class QuickPolymatrixGame(PolymatrixGame):
+    def __init__(self, number_of_players=3, timesteps=0, log_level="warning"):
+        manager = GameManager(number_of_players)
+        players = manager.get_random_players()
+
+        game_settings = {
+            "start_populations_matrix": players,
+            "topology": "fully_connected",
+            'alpha': 1.0,
+            'log_level': log_level
+        }
+        self = super().__init__(**game_settings)
+        self.manager = manager
+        self.game_settings = game_settings
+        self.timesteps = timesteps
+        self.simulate()
+
+    def simulate(self):
+        print(f"Simulated for {len(self.players)} players")
+        print(f" Network topology: {self.game_settings['topology']}")
+        print(f" Game state {self.state.tolist()}")
+        for i in range(self.timesteps):
+            self.play(self.manager.get_random_strategy_profile())
+            print(f" Strategy profile: {self.strategy_profile}")
+            print(f" Game state {self.state.tolist()}")
--- game.py
+++ game.py
@@ -8,29 +8,8 @@ from logger import log
                     log_level=("Preferred level of logging", "option", "log", str),
                     timesteps=("Amount of rounds to be played", "option", "t", int))
 def main(number_of_players=3, timesteps=0, log_level="warning"):
-    #log.setLevel(log_level)
-    #print(log.handlers, log.handler)
-    manager = corpgame.GameManager(number_of_players)
-    players = manager.get_random_players()
-    # players = [[100, 100], [100, 100], [100, 100], [100, 100]]
-    game_settings = {
-        "start_populations_matrix": players,
-        "topology": "fully_connected",
-        'alpha': 1.0,
-        'log_level': log_level
-    }
-    game = corpgame.PolymatrixGame(**game_settings)
-    log.info(f"Simulated for {number_of_players} players")
-    log.info(f" Network topology: {game_settings['topology']}")
-    print(f" Game state {game.state.tolist()}")
-    for i in range(timesteps):
-        game.play(manager.get_random_strategy_profile())
-        print(f" Strategy profile: {game.strategy_profile}")
-        print(f" Game state {game.state.tolist()}")
-    game.get_all_actions()
-    from pprint import pprint
-    #pprint(game.actions)
-    #pprint(game.payoffs)
+    corpgame.QuickPolymatrixGame(number_of_players, timesteps, log_level)
+
 if __name__ == "__main__":
     plac.call(main)
     
