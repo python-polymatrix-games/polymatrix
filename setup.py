6c70dc3b911b125ee68efa6ba296c88b9174897f
Oskar Person <oskar.person@hotmail.com>
2021-03-01 21:26:26 +0100

    installation attempt:

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setuptools.setup(
+    name="polymatrix",
+    version="0.0.1",
+    description="Tools for simulating and solving polymatrix games.",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Oskar Person",
+    author_email="",
+    license="MIT License",
+    packages=setuptools.find_packages(exclude=['tests']),
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "scikit-learn>=0.22.2",
+        "pandas>=1.0.0",
+        "matplotlib>=3.1.1",
+        "scipy>=1.4.0",
+        "joblib>=0.13.2",
+        "tqdm>=4.41.0",
+        "shap>=0.36.0",
+        "numpy>=1.19.0"
+    ],
+    url="https://github.com/python-polymatrix-games/polymatrix-games",
+    zip_safe=False,
+)
